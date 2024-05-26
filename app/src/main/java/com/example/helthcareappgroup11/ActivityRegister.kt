ad0aecf96064f2794e238662ef8be36979f0e7ff
Hardik Thakor <thakorhardik2851@gmail.com>
2024-05-26 17:14:42 -0400

    Login and Registration implemented successfully

--- app/src/main/java/com/example/helthcareappgroup11/ActivityRegister.kt
+++ app/src/main/java/com/example/helthcareappgroup11/ActivityRegister.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.TextView
@@ -24,8 +25,9 @@ class ActivityRegister : AppCompatActivity() {
         val radioGroup = findViewById<RadioGroup>(R.id.radioGroupRole)
         val userRole = findViewById<RadioButton>(R.id.user_role)
         val docterRole = findViewById<RadioButton>(R.id.docter_role)
-        val email = findViewById<TextView>(R.id.email)
-        val password = findViewById<TextView>(R.id.password)
+        val email = findViewById<EditText>(R.id.email)
+        val password = findViewById<EditText>(R.id.password)
+        val confirmPassword = findViewById<EditText>(R.id.confirmPassword)
 
         val btnRegister = findViewById<Button>(R.id.btnRegister)
 
@@ -44,24 +46,37 @@ class ActivityRegister : AppCompatActivity() {
         btnRegister.setOnClickListener {
             val emailText = email.text.toString()
             val passwordText = password.text.toString()
+            val confirmPasswordText = confirmPassword.text.toString()
 
             Log.d("RegistrationDetails", "Role: $role")
             Log.d("RegistrationDetails", "Email: $emailText")
             Log.d("RegistrationDetails", "Password: $passwordText")
 
-            auth.createUserWithEmailAndPassword(emailText, passwordText)
-                .addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        val userId = auth.currentUser?.uid
-                        val userRoleRef = FirebaseDatabase.getInstance().reference.child("users").child(userId!!)
-                        userRoleRef.setValue(UserRole(role))
+            if(passwordText == confirmPasswordText) {
+                auth.createUserWithEmailAndPassword(emailText, passwordText)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            val userId = auth.currentUser?.uid
+                            val userRoleRef =
+                                FirebaseDatabase.getInstance().reference.child("users")
+                                    .child(userId!!)
+                            userRoleRef.setValue(UserRole(role))
 
-                        Toast.makeText(this, "Registration successful!", Toast.LENGTH_SHORT).show()
-                    } else {
-                        Toast.makeText(this, "Registration failed: ${task.exception?.message.toString()}", Toast.LENGTH_SHORT).show()
-                        Log.d("ERROR", task.exception.toString())
+                            Toast.makeText(this, "Registration successful!", Toast.LENGTH_SHORT)
+                                .show()
+                        } else {
+                            Toast.makeText(
+                                this,
+                                "Registration failed: ${task.exception?.message.toString()}",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            Log.d("ERROR", task.exception.toString())
+                        }
                     }
-                }
+            }else{
+                Toast.makeText(this, "Password must match with confirm password", Toast.LENGTH_SHORT)
+                    .show()
+            }
 
 //            Toast.makeText(this, "Registration successful!", Toast.LENGTH_SHORT).show()
         }
