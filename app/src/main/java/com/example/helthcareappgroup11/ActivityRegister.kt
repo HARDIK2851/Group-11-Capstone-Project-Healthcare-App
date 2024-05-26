881901874c7dff1953eeb3be1bc26d0faf6bb4a6
Hardik Thakor <thakorhardik2851@gmail.com>
2024-05-25 22:48:12 -0400

    register checked

--- app/src/main/java/com/example/helthcareappgroup11/ActivityRegister.kt
+++ app/src/main/java/com/example/helthcareappgroup11/ActivityRegister.kt
@@ -1,11 +1,50 @@
 package com.example.helthcareappgroup11
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
+import android.widget.Toast
 
 class ActivityRegister : AppCompatActivity() {
+    private lateinit var role: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val radioGroup = findViewById<RadioGroup>(R.id.radioGroupRole)
+        val userRole = findViewById<RadioButton>(R.id.user_role)
+        val docterRole = findViewById<RadioButton>(R.id.docter_role)
+        val email = findViewById<TextView>(R.id.email)
+        val password = findViewById<TextView>(R.id.password)
+
+        val btnRegister = findViewById<Button>(R.id.btnRegister)
+
+        role = if (userRole.isChecked) "user" else "doctor"
+
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            role = when (checkedId) {
+                R.id.user_role -> "user"
+                R.id.docter_role -> "doctor"
+                else -> ""
+            }
+            // You can perform additional actions with the selected role here
+        }
+
+        btnRegister.setOnClickListener {
+            val emailText = email.text.toString()
+            val passwordText = password.text.toString()
+
+            Log.d("RegistrationDetails", "Role: $role")
+            Log.d("RegistrationDetails", "Email: $emailText")
+            Log.d("RegistrationDetails", "Password: $passwordText")
+
+            Toast.makeText(this, "Registration successful!", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
--- app/src/main/java/com/example/helthcareappgroup11/MainActivity.kt
+++ app/src/main/java/com/example/helthcareappgroup11/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.helthcareappgroup11
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val delay: Long = 2000
+
+        Handler().postDelayed({
+            // Start the new activity
+            val intent = Intent(this, ActivityLogin::class.java)
+            startActivity(intent)
+            finish() // Optional: Close the current activity
+        }, delay)
     }
 }
