bd435454b35c71b1a738b2c99612bf5681030b7b
mrGavy <gabyjudge07@gmail.com>
2024-05-27 21:23:09 -0400

    Bottom Navigation created by me(Jaspreet) I have created the bottom navigation with four empty fragments home, doctor's profile, setting, upcoming appointment and adding appointment, and I added relevant icons as well. Added the background image for the main screen which resembles as logo

--- app/src/main/java/com/example/helthcareappgroup11/HomeActivityDoctor.kt
+++ app/src/main/java/com/example/helthcareappgroup11/HomeActivityDoctor.kt
@@ -5,16 +5,59 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.helthcareappgroup11.fragments.AddAppointmentFragment
+import com.example.helthcareappgroup11.fragments.DoctorProfileFragment
+import com.example.helthcareappgroup11.fragments.HomeFragment
+import com.example.helthcareappgroup11.fragments.SettingFragment
+import com.example.helthcareappgroup11.fragments.UpcomingFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivityDoctor : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_home_doctor)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId){
+                R.id.bottom_home -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+
+                R.id.bottom_doctorProfile -> {
+                    replaceFragment(DoctorProfileFragment())
+                    true
+                }
+
+                R.id.bottom_settings -> {
+                    replaceFragment(SettingFragment())
+                    true
+                }
+
+                R.id.bottom_adding_appointment -> {
+                    replaceFragment(AddAppointmentFragment())
+                    true
+                }
+
+                R.id.bottom_upcoming -> {
+                    replaceFragment(UpcomingFragment())
+                    true
+                }
+
+                else -> false
+            }
         }
+
+        replaceFragment(HomeFragment())
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.doctor_container, fragment).commit()
     }
 }
