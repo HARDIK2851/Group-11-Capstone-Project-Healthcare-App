185cec5fe8756c843bcbba47394966e98c412569
Hardik Thakor <thakorhardik2851@gmail.com>
2024-06-15 18:48:30 -0400

    complete doctor profile and add clinic functionality added

--- app/src/main/java/com/example/helthcareappgroup11/objectClasses/Clinics.kt
+++ app/src/main/java/com/example/helthcareappgroup11/objectClasses/Clinics.kt
@@ -1,3 +1,11 @@
 package com.example.helthcareappgroup11.objectClasses
 
-data class Clinics(var clinicName: String?)
+data class Clinics(
+    var clinicName: String?,
+    var streetAddress: String?,
+    var city: String?,
+    var zipcode: String?,
+    var contactNumber: String?,
+    var doctorId: String,
+    var photoUrls: ArrayList<String>
+)
--- app/src/main/java/com/example/helthcareappgroup11/objectClasses/Doctors.kt
+++ app/src/main/java/com/example/helthcareappgroup11/objectClasses/Doctors.kt
@@ -1,3 +1,13 @@
 package com.example.helthcareappgroup11.objectClasses
 
-data class Doctors(var name: String?)
+data class Doctors(
+    var fullName: String?,
+    var email: String?,
+    var phone: String?,
+    var clinicId: String?,
+    var specialization: String?,
+    var education: String?,
+    var certifications: String?,
+    var bio: String?,
+    var photoUrl: String?
+)
