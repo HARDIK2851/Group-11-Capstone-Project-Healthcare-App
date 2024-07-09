a13407c8d64ecec88797fa975489594cf0736f1a
Hardik Thakor <thakorhardik2851@gmail.com>
2024-07-09 14:09:48 -0400

    Reset the main activity code to orignal one

--- app/src/main/java/com/example/helthcareappgroup11/MainActivity.kt
+++ app/src/main/java/com/example/helthcareappgroup11/MainActivity.kt
@@ -10,64 +10,14 @@ import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var auth: FirebaseAuth
-    private lateinit var database: FirebaseDatabase
-    private lateinit var userRoleRef: DatabaseReference
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        auth = FirebaseAuth.getInstance()
-        database = FirebaseDatabase.getInstance()
-
         // Delay execution by 2 seconds
         Handler().postDelayed({
-            checkCurrentUser()
-        }, 2000) // 2000 milliseconds = 2 seconds
+            val intent = Intent(this@MainActivity, ActivityLogin::class.java)
+            startActivity(intent)
+        }, 2000)
     }
 
-    private fun checkCurrentUser() {
-        val currentUser = auth.currentUser
-
-        if (currentUser != null) {
-            // User is signed in, fetch user role
-            userRoleRef = database.reference.child("users").child(currentUser.uid).child("role")
-            checkUserAndNavigate()
-        } else {
-            // No user is signed in, redirect to login
-            goToLogin()
-        }
-    }
-
-    private fun checkUserAndNavigate() {
-        userRoleRef.addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val userRole = snapshot.getValue(String::class.java)
-
-                if (userRole == "doctor") {
-                    val intent = Intent(this@MainActivity, DoctorFirstStepActivity::class.java)
-                    startActivity(intent)
-                } else if (userRole == "user") {
-                    val intent = Intent(this@MainActivity, HomeActivityUser::class.java)
-                    startActivity(intent)
-                } else {
-                    // Handle unknown or undefined roles
-                    goToLogin()
-                }
-                finish()
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                // Handle database error
-                goToLogin()
-            }
-        })
-    }
-
-    private fun goToLogin() {
-        val intent = Intent(this@MainActivity, ActivityLogin::class.java)
-        startActivity(intent)
-        finish()
-    }
 }
