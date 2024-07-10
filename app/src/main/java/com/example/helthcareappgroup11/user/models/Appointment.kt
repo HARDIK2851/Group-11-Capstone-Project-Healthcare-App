4695931b34962948a9460befd9b9938f6bf6627a
Jaspreet <arjproject1group8@gmail.com>
2024-07-10 17:00:43 -0400

    checking if project working properly

---
+++ app/src/main/java/com/example/helthcareappgroup11/user/models/Appointment.kt
@@ -0,0 +1,6 @@
+data class Appointment(
+    val doctorImage: Int,  // Use Int if using drawable resource IDs
+    val doctorName: String,
+    val doctorExpertise: String,
+    val appointmentDateTime: String
+)
