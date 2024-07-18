760a87002f08e8ae8d9fa4e3f3cfea87a4e4bfd5
Jaspreet <deepag1111@gmail.com>
2024-07-18 03:39:10 -0400

    one more activity for user, where users will come and enter their credientials in first step user activity, done by deepa

--- app/src/main/java/com/example/helthcareappgroup11/ActivityLogin.kt
+++ app/src/main/java/com/example/helthcareappgroup11/ActivityLogin.kt
@@ -8,6 +8,7 @@ import android.widget.EditText
 import android.widget.Toast
 import com.example.helthcareappgroup11.doctor.activities.DoctorFirstStepActivity
 import com.example.helthcareappgroup11.doctor.objectClasses.UserRole
+import com.example.helthcareappgroup11.user.activities.FirstStepUserActivity
 import com.example.helthcareappgroup11.user.activities.HomeActivityUser
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
