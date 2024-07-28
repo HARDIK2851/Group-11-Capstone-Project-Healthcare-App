bc7ea19fb16739d9da364622d550c68262f8ffe8
Hardik Thakor <thakorhardik2851@gmail.com>
2024-07-28 13:45:43 -0400

    Clinic list fixed

--- app/src/main/java/com/example/helthcareappgroup11/user/activities/HomeActivityUser.kt
+++ app/src/main/java/com/example/helthcareappgroup11/user/activities/HomeActivityUser.kt
@@ -8,6 +8,7 @@ import com.example.helthcareappgroup11.user.fragments.AccountFragmentUser
 import com.example.helthcareappgroup11.user.fragments.AppointmentFragment
 import com.example.helthcareappgroup11.user.fragments.NotificationFragmentUser
 import com.example.helthcareappgroup11.user.fragments.UserHomeFragment
+import com.example.helthcareappgroup11.user.fragments.doctorClinicUserFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivityUser : AppCompatActivity() {
@@ -22,7 +23,7 @@ class HomeActivityUser : AppCompatActivity() {
             var selectedFragment: Fragment? = null
             when (item.itemId) {
                 R.id.nav_user_home -> selectedFragment = UserHomeFragment()
-                R.id.nav_user_clinic -> selectedFragment = UserHomeFragment()
+                R.id.nav_user_clinic -> selectedFragment = doctorClinicUserFragment()
                 R.id.nav_user_appointment -> selectedFragment = AppointmentFragment()
                 R.id.nav_user_notification -> selectedFragment = NotificationFragmentUser()
                 R.id.nav_user_account -> selectedFragment = AccountFragmentUser()
